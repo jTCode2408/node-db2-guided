6cbc5f79c7bea6ca4943841cc91a5bd76be8577b
jTCode2408 <jasmine.terry91@gmail.com>
2020-02-18 22:33:55 -0500

    end of lecture

---
+++ knexfile.js
@@ -0,0 +1,46 @@
+// Update with your config settings.
+//create migrations folder
+//create new db
+module.exports = {
+
+  development: {
+    client: 'sqlite3',
+    connection: {
+      filename: './data/got.db3'
+    },
+    useNullAsDefault: true //only needed for SQLite3
+  },
+
+  staging: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+
+  production: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
+
+};
